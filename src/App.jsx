befd6f7dd58155677ba8c50d6399bbe399718343
Naman Singla <namansingla830@gmail.com>
2025-11-05 22:48:45 +0530

    connect all route

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Service from "./pages/Service";
+import Pages from "./pages/Pages";
+import Blog from "./pages/Blog";
+import Projects from "./pages/Projects";
 
 export default function App() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <MainLayout>
-            <Home />
-          </MainLayout>
-        }
-      />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="service" element={<Service />} />
+        <Route path="pages" element={<Pages />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="projects" element={<Projects />} />
+      </Route>
     </Routes>
   );
 }
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,14 @@
 import Navbar from "../components/common/Navbar";
 import Footer from "../components/common/Footer";
+import { Outlet } from "react-router-dom";
 
-export default function MainLayout({ children }) {
+export default function MainLayout() {
   return (
     <>
       <Navbar />
-      <main className="flex-grow pt-20">{children}</main>
+      <main className="flex-grow pt-20">
+        <Outlet />
+      </main>
       <Footer />
     </>
   );
