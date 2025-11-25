a48036bc612ede55b9269a32901294cd96bacec6
Naman Singla <namansingla830@gmail.com>
2025-11-25 17:47:09 +0530

    project page fixed

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,6 @@
 import { ProjectsComponent } from "../components/CommonSection/Sections";
 import Hero from "../components/common/Hero";
 
-
 const Projects = () => {
   return (
     <>
@@ -11,7 +10,11 @@ const Projects = () => {
         paragraph="Showcasing the Power of Innovative, Results-Driven Digital Solutions in Action."
         pageName="Projects"
       />
-      <ProjectsComponent />
+      <section className="w-full md:py-24 py-16 bg-gray-50">
+        <div className="lg:container w-full mx-auto px-4">
+          <ProjectsComponent />
+        </div>
+      </section>
     </>
   );
 };
